a1eab935eb6f09100ac264eb96cbfeb7c1c1dcba
Axel <axel1717@gmail.com>
2020-11-22 00:37:33 +0300

    50% of movie details screen

--- app/src/main/java/ru/ekhalikov/homework2/MainActivity.kt
+++ app/src/main/java/ru/ekhalikov/homework2/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.ac_movie_info)
         supportActionBar?.title = "Movies List"
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(R.drawable.ic_actionbar_logo)
