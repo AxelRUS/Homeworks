512a259fd6fe001d91b1b6d7cbbcef02a6e4cf1e
Axel <axel1717@gmail.com>
2020-12-01 00:42:30 +0300

    clean code

--- app/src/main/java/ru/ekhalikov/homework2/MainActivity.kt
+++ app/src/main/java/ru/ekhalikov/homework2/MainActivity.kt
@@ -2,21 +2,13 @@ package ru.ekhalikov.homework2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity(), MovieListFragment.OnCardClick,
     MovieDetailsFragment.BackButtonListener {
 
-    var container: FragmentContainerView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        container = findViewById(R.id.container)
-
-
-//        supportActionBar?.setDisplayShowHomeEnabled(true)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -27,14 +19,12 @@ class MainActivity : AppCompatActivity(), MovieListFragment.OnCardClick,
 
     override fun onClick() {
         supportFragmentManager.beginTransaction()
-//                .addToBackStack(null)
             .replace(R.id.container, MovieDetailsFragment.newInstance())
             .commit()
     }
 
     override fun onBackButtonPressed() {
         supportFragmentManager.beginTransaction()
-//            .addToBackStack(null)
             .replace(R.id.container, MovieListFragment.newInstance())
             .commit()
     }
