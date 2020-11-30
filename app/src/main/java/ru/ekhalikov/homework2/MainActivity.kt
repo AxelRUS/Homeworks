8679c908ef28cffbcaa05aae4e8bd9f8c6ad4244
Axel <axel1717@gmail.com>
2020-11-30 19:23:53 +0300

    rename fragments

--- app/src/main/java/ru/ekhalikov/homework2/MainActivity.kt
+++ app/src/main/java/ru/ekhalikov/homework2/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ac_movie_info)
+        setContentView(R.layout.fr_movie_details)
         supportActionBar?.title = "Back"
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(R.drawable.ic_actionbar_logo)
