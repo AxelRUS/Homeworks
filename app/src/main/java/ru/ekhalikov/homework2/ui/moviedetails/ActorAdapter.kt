3ac0f9b1dd97ec17bb86e57f1e1653101b2560ad
Axel <axel1717@gmail.com>
2021-02-07 12:45:14 +0300

    refactor adapter & viewholder classes

--- app/src/main/java/ru/ekhalikov/homework2/ui/moviedetails/ActorAdapter.kt
+++ app/src/main/java/ru/ekhalikov/homework2/ui/moviedetails/ActorAdapter.kt
@@ -1,11 +1,14 @@
 package ru.ekhalikov.homework2.ui.moviedetails
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ru.ekhalikov.homework2.R
 
-class ActorAdapter: RecyclerView.Adapter<ActorViewHolder>() {
+class ActorAdapter : RecyclerView.Adapter<ActorAdapter.ActorViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.view_holder_actor, parent, false)
@@ -18,5 +21,14 @@ class ActorAdapter: RecyclerView.Adapter<ActorViewHolder>() {
 
     override fun getItemCount(): Int {
         return 10;
+    }
+
+    class ActorViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val image = item.findViewById<ImageView>(R.id.image)
+        val name = item.findViewById<TextView>(R.id.name)
+
+        fun bind() {
+
+        }
     }
 }
--- app/src/main/java/ru/ekhalikov/homework2/ui/movies/MovieAdapter.kt
+++ app/src/main/java/ru/ekhalikov/homework2/ui/movies/MovieAdapter.kt
@@ -3,19 +3,21 @@ package ru.ekhalikov.homework2.ui.movies
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ru.ekhalikov.homework2.R
 import ru.ekhalikov.homework2.model.Movie
 
-class MovieAdapter(var onClickListener: View.OnClickListener): RecyclerView.Adapter<MovieViewHolder>() {
+class MovieAdapter(var onClickListener: View.OnClickListener) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
-    private var movies: List<Movie> = emptyList<Movie>()
+    private var movies: List<Movie> = emptyList()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.view_holder_movie, parent, false)
         view.setOnClickListener(onClickListener)
-        return MovieViewHolder(view)
+        return ViewHolder(view)
     }
 
     fun setData(movies: List<Movie>) {
@@ -23,11 +25,23 @@ class MovieAdapter(var onClickListener: View.OnClickListener): RecyclerView.Adap
         notifyDataSetChanged()
     }
 
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(movies[position])
     }
 
     override fun getItemCount(): Int {
         return movies.size;
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        var image: ImageView = view.findViewById(R.id.ivMovie)
+        var title: TextView = view.findViewById(R.id.tvMovieName)
+        var length: TextView = view.findViewById(R.id.tvMovieLength)
+
+        fun bind(movie: Movie) {
+//        image.load()
+            title.text = movie.title
+        }
     }
 }
