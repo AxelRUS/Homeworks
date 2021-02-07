3a12f3a7d6fcdcb6c733800244451f41f4fd780e
Axel <axel1717@gmail.com>
2021-02-07 10:29:49 +0300

    added data & model files

---
+++ app/src/main/java/ru/ekhalikov/homework2/data/JsonGenre.kt
@@ -0,0 +1,6 @@
+package ru.ekhalikov.homework2.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal class JsonGenre(val id: Int, val name: String)
---
+++ app/src/main/java/ru/ekhalikov/homework2/model/Genre.kt
@@ -0,0 +1,3 @@
+package ru.ekhalikov.homework2.model
+
+data class Genre(val id: Int, val name: String)
